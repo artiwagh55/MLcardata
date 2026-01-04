c6e83b9540cc1a14cd7f6468fb2c6a2211dcc4dc
arti55 <artiwagh555@gmail.com>
2026-01-04 22:59:54 +0530

    Fix app run for Render deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,6 @@ def predict():
     return f"Predicted MPG: {prediction[0]:.2f}"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8000)
+    # ONLY ONE RUN for Render
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
